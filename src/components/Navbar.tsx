b88a7430bcaa073f2c5f575f9d05eeba81d2b7c8
shifachaus <chausshifa804@gmail.com>
2024-03-03 19:33:32 +0530

    add contact route with BackgroundBeams effect

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -22,11 +22,11 @@ const Navbar = ({ className }: { className?: string }) => {
 
         <MenuItem setActive={setActive} active={active} item="Our Courses">
           <div className="flex flex-col space-y-4 text-sm">
-            <HoveredLink href="/course">All Courses</HoveredLink>
-            <HoveredLink href="/course">Basic Music Theory</HoveredLink>
-            <HoveredLink href="/course">Adavanced Composition</HoveredLink>
-            <HoveredLink href="/course">Songwriting</HoveredLink>
-            <HoveredLink href="/course">Music Production</HoveredLink>
+            <HoveredLink href="/courses">All Courses</HoveredLink>
+            <HoveredLink href="/courses">Basic Music Theory</HoveredLink>
+            <HoveredLink href="/courses">Adavanced Composition</HoveredLink>
+            <HoveredLink href="/courses">Songwriting</HoveredLink>
+            <HoveredLink href="/courses">Music Production</HoveredLink>
           </div>
         </MenuItem>
 
